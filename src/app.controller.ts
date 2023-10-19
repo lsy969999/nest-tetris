55790397f0436ffa2978a1bea5e2938b5b64675a
seongyun.lim <seongyun.lim@e4net.net>
2023-10-19 13:35:06 +0900

    .

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index.hbs')
+  @Render('index')
   getHello() {
     return {
       message: 'Hello World!'
