95bfa8069818a7555ddadf1b1b5aff843a0f1f82
seongyun.lim <seongyun.lim@e4net.net>
2023-10-23 19:13:35 +0900

    ..

--- bck/src/websock/brick.enum.ts
+++ bck/src/websock/brick.enum.ts
@@ -1,12 +1,42 @@
-export enum Brick {
-  I,
-  J,
-  L,
-  O,
-  S,
-  T,
-  Z,
+export enum BrickType {
+  I = 0,
+  J = 1,
+  L = 2,
+  O = 3,
+  S = 4,
+  T = 5,
+  Z = 6,
 }
+
+export enum BrickStatus {
+  SETTED = 0,
+  DROPPING = 1,
+  CREATING = 2,
+}
+
+export enum BrickPosition {
+  UP = 0,
+  DOWN = 1,
+  LEFT = 2,
+  RIGHT = 3,
+}
+
+export enum BrickColor {
+  YELLOW = 0,
+  SKY = 1,
+  BLUE = 2,
+  ORANGE = 3,
+  RED = 4,
+  GREEN = 5,
+  PURPLE = 6,
+}
+
+export enum BrickReserved {
+
+}
+
+export type Brick = [BrickType, BrickStatus, BrickPosition, BrickColor, BrickReserved];
+
 /**
  * Brick.I
  * [
